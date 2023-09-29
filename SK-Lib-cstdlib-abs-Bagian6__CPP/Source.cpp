41c1c1c9565d79d8cc58b175af6f1b6be73fd2b5
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-09-29 22:01:25 +0700

    Initial commit

---
+++ SK-Lib-cstdlib-abs-Bagian6__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+
+/*
+	Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+using namespace std;
+
+int main() {
+	cout << "abs(10) = " << abs(10) << "\n";
+	cout << "abs(-10) = " << abs(-10) << "\n";
+	cout << "abs(50) = " << abs(-50) << "\n";
+	cout << "abs(-50) = " << abs(50) << "\n";
+
+	_getch();
+	return 0;
+}
